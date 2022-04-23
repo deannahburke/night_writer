7cbf1e0d86a49886adb4e425dd1d96dd62876dfc
Deannah Burke <deannah@bierstadtlager.com>
2022-04-23 10:24:07 -0600

    Finish english to braille hash, test reading of keys and values

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe Dictionary do
     expect(dictionary.to_braille).to be_a(Hash)
   end
 
+  it "can read alphabet hash" do
+    dictionary = Dictionary.new
+
+    expect(dictionary.to_braille['a']).to eq(["0.", "..", ".."])
+    expect(dictionary.to_braille['k']).to eq(["0.", "..", "0."])
+    expect(dictionary.to_braille['u']).to eq(["0.", "..", "00"])
+    expect(dictionary.to_braille[" "]).to eq(["..", "..", ".."])
+  end
 
 end
